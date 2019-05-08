52002aa5567856f059b3c3f7061b8e139c6f1ce5
zhonghua <zhonghua@bogon.pwrd.com>
2019-05-08 19:28:16 +0800

    to makefile from python

--- Makefile
+++ Makefile
@@ -1,40 +1,43 @@
+.PHONY: clean all init
+
 TEMP := temp
 TARGET := goldcaptain
-MAKE_DEP := $(TEMP)/make_dep.d
 DIRS = .
+
+all: $(TARGET)
 
 CPPFLAGS := $(shell sdl2-config --cflags)
 CPPLIBS := $(shell sdl2-config --libs) -lfreetype -lSDL2_mixer -lSDL2_image
 
-define TEMP_O
-TEMP_FILE = $(TEMP)/$(subst /,., $(1)).o
-$(shell g++ -MM $(CPPFLAGS) $(1))
-endef
+#get all srcs and remove ./
+SRC := $(subst ./,,$(foreach DIR,$(DIRS),$(wildcard $(DIR)/*.cpp)))
 
-#get all srcs
-SRC := $(foreach DIR,$(DIRS),$(wildcard $(DIR)/*.cpp))
-#remove ./
-SRC := $(subst ./,,$(SRC))
+#temp/util.cpp.o:util.cpp
+#	g++ -c $(CPPFLAGS) $< -o $@
 
 #replace slash(/) to dot(.)
 SRC_SLASH := $(subst /,., $(SRC))
 
 #temporary files under temp dir
-OBJECTS := $(TEMP)/$($(SRC_SLASH): %=%.o)
+OBJECTS := $(patsubst %.cpp,%.o,$(foreach ONE,$(SRC_SLASH),$(TEMP)/$(ONE)))
 
-
-goldcaptain:$(OBJECTS)
+$(TARGET):$(OBJECTS)
 	g++ -o $@ $(OBJECTS) $(CPPLIBS)
 
-
-$(foreach CPP,$(SRC),$(eval $(call TEMP_o,$(CPP))))
-
 #check if make target is default
-#ifeq ($(MAKECMDGOALS),)
-#include $(OBJECTS_DEPS)
-#endif
+ifeq ($(MAKECMDGOALS),)
+include temp/temp.d
+endif
 
-.PHONY: clean
+define TEMP_O
+$(eval CPP_O = $(patsubst %.cpp,%.o,$(TEMP)/$(subst /,.,$(1))))
+$(shell g++ -MM $(CPPFLAGS) $(1) | sed -e "s|^.*\.o[ :]*|$(CPP_O):|g" >> temp/temp.d)
+$(shell echo "\n" >> temp/temp.d)
+endef
+
+init:
+	rm -f temp/temp.d; \
+$(foreach CPP,$(SRC),$(call TEMP_O,$(CPP)))
 
 clean:
 	rm -f *.d; \
--- make_dep.py
+++ make_dep.py
@@ -3,22 +3,56 @@
 import sys
 import os
 
-#argv = dir list
+#all src dirs
+dirs = "."
+temp_dir = "temp"
+target = "goldcaptain"
+cpp_flags = "$(shell sdl2-config --cflags)"
+cpp_libs = "$(shell sdl2-config --libs) -lfreetype -lSDL2_mixer -lSDL2_image"
 
+############################
+#create tempdir
+if not os.path.exists(temp_dir):
+	os.make_dirs(temp_dir)
+	
+#########################################
+#split line
 cpp_files = []
-for arg_dir in sys.argv[1:]:
+for one_dir in dirs.split(' '):
 	cpp_files = cpp_files + [cpp for cpp in os.listdir(arg_dir) if cpp.endswith('.cpp') or cpp.endswith('.c')]
 
-cmd = """
-	rm -f $@; \\n
-	g++ -MM $(CPPFLAGS) $< > #@.$$$$; \\n
-	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \\n
-	rm -f $@.$$$$\n
-"""
+objs = list(map(lambda cpp:temp_dir+"/"+cpp.replace(".cpp",".o").replace("/","."), cpp_files))
 
-with open("temp/make_dep.d", "a") as make_dep:
-	for cpp in cpp_files:
-		dep_file = "temp/"+cpp.replace("/",".")+".d"
-		make_dep.write(dep_file+":"+cpp+"\n")
-		make_dep.write(cmd+"\n\n")
+phony = """
+.PHONY: clean all
+all:$(TARGET)
+
+""".replace("$(TARGET)", target)
+
+flags = """
+
+CPPFLAGS := $(cpp_flags)
+CPPLIBS := $(cpp_libs)
+
+""".replace("$(cpp_flags)", cpp_flags).replace("$(cpp_libs)", cpp_libs)
+
+clean_cmd = """
+
+clean:
+	rm -f *.d; \
+	rm -f *.o; \
+	rm -f $(TARGET)
+
+""".replace("$(TARGET)", target)
+
+target_cmd = """
+$(TARGET):$(OBJS)
+	g++ -o $@ $< $(CPPLIBS)
+
+""".replace("$(TARGET)", target).replace("$(OBJS)", objs.join(" "))
+
+with open("Makefile", "a") as make_file:
+	make_file.write(phony)
+	make_file.write(flags)
+	make_file.write(clean_cmd)
 	
