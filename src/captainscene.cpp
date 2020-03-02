b3a56481c073060d6c6308ebc83d2bb2e9729c4d
zhonghua <zhonghua@bogon.pwrd.com>
2020-03-02 20:57:58 +0800

    run ok

---
+++ src/captainscene.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+
+#include "captainscene.h"
+
+
+	// Minimum virtual display size for portrait orientation
+
+float CaptainScene::defaultZoom = 0;
+float CaptainScene::minZoom;
+float CaptainScene::maxZoom;
+
+Camera* CaptainScene::uiCamera;
+
+CaptainScene::CaptainScene()
+{
+}
+
+CaptainScene::~CaptainScene()
+{
+
+}
+
+void CaptainScene::init()
+{
+	
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "goldcaptain.h"
+#include "captainscene.h"
 
 int main(int argc, char** argv)
 {
 	GoldCaptain gd;
 	gd.init();
 
+	gd.switchScene(new CaptainScene());
 	gd.run();
 	
 	gd.finish();
